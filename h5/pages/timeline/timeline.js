a4e4b5f8069bbf42a5fd92067bfb0dcac8b96c84
chenshunliang <shunliangc@jumei.com>
2018-03-09 18:03:43 +0800

    设置覆盖地图组件  新增时光机背景音乐 优化展示

--- h5/pages/timeline/timeline.js
+++ h5/pages/timeline/timeline.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    src: 'http://music.163.com/song/media/outer/url?id=16431885.mp3',
+    music_status: true,
+    play_class: 'music_animation',
+    cover_url: 'https://p1.music.126.net/hXzBNrAtSr_3PT-8tJwthw==/19229358858426764.jpg',
+    title: 'tomorrow'
   },
 
   /**
@@ -19,7 +23,22 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    wx.playBackgroundAudio({
+      dataUrl: this.data.src,
+      coverImgUrl: this.data.cover_url,
+      title: this.data.title
+    })
+    const that = this
+    wx.onBackgroundAudioPause(function () {
+      that.setData({
+        music_status: false
+      });
+    })
+    wx.onBackgroundAudioPlay(function () {
+      that.setData({
+        music_status: true
+      });
+    })
   },
 
   /**
@@ -62,5 +81,23 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  playOrStop: function () {
+    const status = this.data.music_status
+    if (status) {
+      wx.pauseBackgroundAudio()
+      this.setData({
+        play_class: ''
+      });
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: this.data.src,
+        coverImgUrl: this.data.cover_url,
+        title: this.data.title
+      })
+      this.setData({
+        play_class: 'music_animation'
+      });
+    }
   }
 })
